48d218d33f9c1cae9aaef346b587dfd121161cdf
TamariPopkhadze <124593153+TamariPopkhadze@users.noreply.github.com>
2023-03-17 04:16:57 +0400

    Add files via upload

---
+++ nutrition.py
@@ -0,0 +1,7 @@
+def main():
+ calories = {"Apple": 130, "Banana": 110, "Avocado": 50, "Cantaloupe": 50, "Grapefruit": 60, "Tangerine": 50,
+"Grapes":90,"Honeydew Melon":50, "Kiwifruit":90, "Lemon":15, "Lime":20, "Strawberiies":50, "Watermelon":80,
+"Nectarine":60, "Orange": 80, "Peach":60, "Pear":100, "Pineapple":50, "Plums": 70,"Sweet Cherries": 100,  }
+ fruit = input("Item: ")
+ print("Calories:" , calories[fruit])
+main()
